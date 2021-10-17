11eeba1f3b6b903d0bef0ed0ccd9e644b15ea10f
LFFloresC <68397899+LFFloresC@users.noreply.github.com>
2021-10-17 10:17:09 -0500

    cambio de server

--- src/utils/ServerIO.js
+++ src/utils/ServerIO.js
@@ -3,8 +3,8 @@ import io from "socket.io-client";
 import Peer from "simple-peer";
 
 const connectSocketServer = () => {
-  // const socket = io.connect("https://savips.herokuapp.com");
-  const socket = io.connect("http://localhost:8000");
+  const socket = io.connect("https://savips.herokuapp.com");
+  // const socket = io.connect("http://localhost:8000");
   socket.emit("client", "pepe");
   return socket;
 };
